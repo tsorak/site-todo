4fab4cc20d9718758ac8ce4973a21174f3d12f21
tsorak <karost0249@gmail.com>
2023-03-12 16:30:19 +0100

    Add authentication page

--- src/Auth.svelte
+++ src/Auth.svelte
@@ -1,7 +1,118 @@
 <script lang="ts">
+	import { login, register } from "./auth";
+
 	//
+	let authType = "login";
+	let authError = "";
+
+	function handleAuthTypeChange(event: Event) {
+		const target = event.target as HTMLInputElement;
+		authType = target.value;
+	}
+
+	function handleSubmit(event: Event) {
+		event.preventDefault();
+		const statusElement = document.querySelector("#authErrorElement") as HTMLParagraphElement;
+
+		const form = event.target as HTMLFormElement;
+		const formData = new FormData(form);
+
+		const email = formData.get("email") as string;
+		const password = formData.get("password") as string;
+		const username = formData.get("username") as string;
+
+		if (authType === "login") {
+			login({ email, password }, statusElement);
+		} else {
+			register({ email, password, username }, statusElement);
+		}
+	}
+
+	$: console.log(authType);
 </script>
 
-<main>
-	<h1>Auth</h1>
-</main>
+<h1>Auth</h1>
+<form on:submit={handleSubmit}>
+	<section>
+		<label>
+			<input type="radio" name="auth" value="login" checked on:change={handleAuthTypeChange} />
+			Login
+		</label>
+		<label>
+			<input type="radio" name="auth" value="register" on:change={handleAuthTypeChange} />
+			Register
+		</label>
+	</section>
+	<div>
+		<label for="email">Email</label>
+		<input type="text" id="email" autocomplete="off" />
+	</div>
+	<div>
+		<label for="password">Password</label>
+		<input type="text" id="password" autocomplete="off" />
+	</div>
+	{#if authType === "register"}
+		<div>
+			<label for="username">Username</label>
+			<input type="text" id="username" autocomplete="off" />
+		</div>
+	{/if}
+	<p id="authErrorElement">{authError}</p>
+	<button type="submit">Submit</button>
+</form>
+
+<style lang="sass">
+	form
+		display: flex
+		flex-direction: column
+		gap: 1rem
+
+		section
+			display: flex
+			justify-content: center
+			gap: 4rem
+
+			label
+				font-weight: 500
+				cursor: pointer
+				border: 1px solid #1a1a1a
+				background: #1a1a1a
+				padding: 0.5rem 1rem
+				border-radius: 0.5rem
+
+				transition: border 0.2s ease, color 0.2s ease
+
+				user-select: none
+
+				@media (prefers-color-scheme: light) 
+					background: #f9f9f9
+					border-color: #f9f9f9
+					color: #000
+
+				input
+					display: none
+
+			label:has(:checked)
+				color: #646cff
+				border-color: #646cff
+
+		div
+			display: flex
+			flex-direction: column
+			gap: 0.5rem
+
+			label
+				font-size: 0.8rem
+				user-select: none
+				color: #666
+
+			input
+				padding: 0.5rem
+				border: 1px solid #aaa
+				border-radius: 0.25rem
+				outline: none
+				font-size: 1rem
+
+				&:focus-within
+					border-color: #646cff
+</style>
---
+++ src/auth.ts
@@ -0,0 +1,66 @@
+async function login(payload: { email: string; password: string }, statusElement: HTMLElement) {
+	statusElement.textContent = "Pending...";
+
+	const res = await fetch("/api/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(payload)
+	});
+	handleAuthStatus(res.status, statusElement);
+
+	const data = await res.json();
+	console.log(data);
+
+	return data;
+}
+
+async function register(payload: { email: string; password: string; username: string }, statusElement: HTMLElement) {
+	statusElement.textContent = "Pending...";
+
+	const res = await fetch("/api/register", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(payload)
+	});
+	handleAuthStatus(res.status, statusElement);
+
+	const data = await res.json();
+	console.log(data);
+
+	return data;
+}
+
+function handleAuthStatus(status: number, element: HTMLElement) {
+	switch (status) {
+		case 200:
+			console.log("Login Successful");
+			element.textContent = "Login Successful";
+			break;
+		case 201:
+			console.log("Registration Successful");
+			element.textContent = "Registration Successful";
+			break;
+		case 400:
+			console.log("Invalid Input");
+			element.textContent = "Invalid Input";
+			break;
+		case 401:
+			console.log("Invalid Credentials");
+			element.textContent = "Invalid Credentials";
+			break;
+		case 409:
+			console.log("Email is already in use");
+			element.textContent = "Email is already in use";
+			break;
+		default:
+			console.log("Unexpected Error");
+			element.textContent = "An unexpected error occured...";
+			break;
+	}
+}
+
+export { login, register };
